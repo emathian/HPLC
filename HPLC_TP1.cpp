185ed628833d88096caaaec7e8888285ba9015d2
Emi <emilie.mathian@insa-lyon.fr>
2019-03-09 16:10:21 +0100

    Commentaire et nettoyage de HPLC_TP1.cpp/h

--- HPLC_TP1.cpp
+++ HPLC_TP1.cpp
@@ -1,3 +1,10 @@
+/*--------------------- RESUME ---------------------------------------------------------
+Ce programme rassemble les fonctions et les procédures de l'exercice 1 sur les vecteurs.
+La mise en parréllélisation de ces dernières a également été implémentée. Ce programme a 
+été réalisé pour faciliter l'implémentation et les tests des fonctions et des procédures. 
+---------------------------------------------------------------------------------------*/
+
+/*---------------------IMPORATION DES LIBRAIRIES --------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>     /* srand, rand */
 #include "HPLC_TP1.h"
@@ -7,62 +14,87 @@
 #include <fstream>
 
 using namespace std;
-//#include <omp.h>
 
-/* POUR L'EXECUTION
-Le premier argument se refere à la taille des vecteurs
-Le second au nombre de threads
-Exemple : ./HPLC_TP1 2000000 2
-*/
+/* ################### PARALLELISATION ################# */
+#include <omp.h>
+/* ##################################################### */
 
 
+/*---------------------CONSIGNES D'EXECUTION ----- --------------------------------------
+Pour l'éxution du progrmma merci d'indiquer en premier argument le nombre de core utilisé,
+et en second argument le nombre de runs 
+Exemple : ./HPLC_TP1 2 2000000
+--------------------------------------------------------------------------------------*/
+
+
+/*---------------------PRROGRAMME PRINCIPAL-------------------------------------------*/
 int main(int argc, char** argv) {
 
-	int nb_thread = atoi(argv[2]);
-	//omp_set_num_threads(nb_thread); 
+	srand((int) time(0)); // Générateur de nombre alétoire
+
+	/*-------------- PARALLELISATION ET OPTIONS D'ÉXÉCUTION --------------------------*/
+	int nb_thread = atoi(argv[2]);  // Définition du nombre de core
+	omp_set_num_threads(nb_thread); 
+	int Taille_tab = atoi(argv[1]); // Taille des vecteurs
+	/*--------------------------------------------------------------------------------*/
 	
-	//OMP_SET_NUM_THREADS(1);
-	srand((int) time(0));
-	int Taille_tab = atoi(argv[1]); // Atoi Ascii to integer
 	
+	/*------------------- INTIALISATION DES VECTEURS ---------------------------------*/
 	double* Tab1;
 	double* Tab2;
 	double* Tab_sum;
 
-
+	// Borne de générteur de nombre aléatoire 
 	double min = 0; 
 	double max = 10; //RAND_MAX;
 
-
+	// Allocation mémoire
 	Tab1 = new double[Taille_tab];
 	Tab2 = new double[Taille_tab]; 
 	Tab_sum = new double[Taille_tab] ;
+	/*--------------------------------------------------------------------------------*/
 
-	int before = (clock() *1000 / CLOCKS_PER_SEC);
 
+	/*------------------- TESTS DES FONTIONS ET PROCEDURE ----------------------------*/
+	int before = (clock() *1000 / CLOCKS_PER_SEC); // Initialisation du chronomètre
+
+	// Remplissage des vecteurs de taille = Taille_tab par des nombre aléatoires compris 
+	// entre min et max :
 	fill (Tab1, Taille_tab, min , max);
 	fill (Tab2, Taille_tab, min , max);
+
+	// Affichage des vecteurs :
+	printf("Affichage vecteur 1   :   \n" );
 	display(Tab1, Taille_tab);
+	printf("Affichage vecteur 2   :   \n" );
 	display(Tab2,  Taille_tab);
 
+	// Somme de deux vecteurs :
 	sum_two_vector(Tab_sum , Tab1 , Tab2 ,Taille_tab  , Taille_tab );
-	//display(Tab_sum, Taille_tab);
+	printf("Affichage du vecteur résultant (3) de la somme du vecteur 1 et 2  :  \n" );
+	display(Tab_sum,  Taille_tab);
 
+	// Somme des éléments d'un vecteur :
 	double s  = sum_vector(Tab1 , Taille_tab);
-	printf("Somme du vecteur 1   :  %f \n" , s);
+	printf("Somme des éléments du vecteur 1   :  %f \n" , s);
 	
+	// Multiplication du vecteur par un double
 	multi_vector(Tab_sum , 2 ,  Taille_tab );
+	printf("Affichage du vecteur 3 * 2   :  %f \n" , s);
+	display(Tab_sum,  Taille_tab);
 
-	int after=  (clock() *1000 / CLOCKS_PER_SEC);
-	int diff = after - before;
-	printf("Durée  :  %d \n", diff );
+	int after=  (clock() *1000 / CLOCKS_PER_SEC); // Arrêt du chronomètre
+	int diff = after - before; // Calcul de la durée d'éxécution
+	printf("Durée d'éxécution :  %d \n", diff ); // Affichage de la durée d'éxécution
 
+	// Mémorisation du temps d'éxécution en fonction du nombre de core et de la taille des
+	// vecterus.
   	ofstream myfile;
-  	myfile.open ("somme_deux_vexct_result.txt", fstream::app);
+  	myfile.open ("Resultats_Vecteurs_Tests.txt", fstream::app);
   	myfile <<  nb_thread <<"\t"<< Taille_tab <<"\t"  << diff <<"\t" <<  "\n";
   	myfile.close();
   
-	
+	// Désallocation de la mémoire :	
 	delete [] Tab1;
 	delete [] Tab2;
 	delete [] Tab_sum;
@@ -70,94 +102,118 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-void fill(double* Tab , int Taille_tab, int min, int max )
-{
-	
-	# pragma omp parallel for
-  	//{
-	for (int i =0 ; i< Taille_tab +1 ; i++  ) 
-		{
-		Tab[i] = Random(min, max);
-		}
-  	//}
-}
 
-
-void display(const double* Tab, int Taille_tab)
+/*--------------------- Procédure : display  ------------------------------------------------
+	Cette procédure permet l'affichage d'un vecteur de taille définit.
+--------------------------------------------------------------------------------------------*/
+void display(const double* Tab, const int Taille_tab)
 {
-  int i;
-  printf("[");
-	
-  for (i = 0; i < Taille_tab; i++)
+  printf("["); // Marque le début d'un vecteur	
+  for (int i = 0; i < Taille_tab; i++) 
   {
     printf("%f", Tab[i]);
-
-    /* We add a comma, except for the last element */
+    // Séparation des éléments par une virgule (exception pour le denier élément)
     if (i < Taille_tab-1) 
     {
       printf(", ");
     }
   }
-  printf("]\n");
+  printf("]\n"); // Marque la fin d'un vecteur.
 }
 
-double Random (double min , double max) 
+
+/*--------------------- Procédure : fill --------------------------------------------------
+	Cette procédure permet le remplissage d'un vecteur, de taille défini, par des doubles
+	générés aléatoirement compris entre min et max. La parallélisation de cette opératon est
+	assurée.
+------------------------------------------------------------------------------------------*/
+void fill(double* Tab , const int Taille_tab, const int min, const int max )
+{
+	/* ################### PARALLELISATION ################# */
+	# pragma omp parallel for
+	/* ##################################################### */
+	
+	for (int i =0 ; i< Taille_tab +1 ; i++ ) // Pour i chaque élément du vecteur : 
+		{
+		Tab[i] = Random(min, max); // Appel du générateur de nombre aléatoire
+		}
+}
+
+
+/*--------------------- Fonction : Random  -------------------------------------------------
+	Cette fonction retourne un double généré alétoirement entre les bornes définies par min
+	er max. La fonction Random est appélé par la procédure fill.
+------------------------------------------------------------------------------------------*/
+double Random (const double min , const double max) 
 {	
 	double f = (double)rand () / RAND_MAX;
 	return min + f * (max-min+1); 
 } 
 
-void sum_two_vector(double * v3 , const double * v1 , const double * v2 , const int taille_v1 , const int taille_v2   )
+
+/*--------------------- Procédure : multi_vector (Multiplication du vecteur par un double) --
+	Cette procédure permet de multiplier les éléments d'un vecteur par une constante (a).
+	Cette opération est parallélisée.
+--------------------------------------------------------------------------------------------*/
+void multi_vector(double * v , const double  a ,  const int taille_v )
 
 {	
+	/* ################### PARALLELISATION ################# */
+	# pragma omp parallel for
+	/* ##################################################### */
+ 		for (int i =0 ; i< taille_v  ; i++  ) 
+ 		{
+ 			v[i] = v[i] * a;
+ 		}   	
+}
+
+
+
+/*--------------------- Procédure : sum_two_vector (Somme de deux vecteurs)  ----------------
+	Cette procédure permet de sommer deux vecteurs et retourne les résultats dans un vecteur v3.
+	Cette opération est parallélisée.
+--------------------------------------------------------------------------------------------*/
+void sum_two_vector(double * v3 , const double * v1 , const double * v2 , const int taille_v1 , const int taille_v2   )
+{	
+	// Condition sur la taille des vecteurs v1 et v2 
 	if (taille_v1 != taille_v2 )
 	{
 		printf("Les vecteurs ont des longueurs diffférentes" );
 	}
+
 	else
 	{
 		
-        # pragma omp parallel for 
-	  	//{
+    	/* ################### PARALLELISATION ################# */
+		# pragma omp parallel for
+		/* ##################################################### */
+	  
  		for (int i =0 ; i< taille_v1  ; i++  ) 
- 			{
- 			
- 			v3[i] = v1[i] + v2[i];
- 			}
-	 //	}
+ 		{
+ 			v3[i] = v1[i] + v2[i]; // Somme des éléments des vecteurs v1 et v2
+ 		}
    	}
-
 }
 
-double sum_vector(const double Tab[], int Taille_tab)
+
+/*--------------------- Fonction : sum_vector (Somme des éléments d'un vecteur)  ----------------
+	Cette fonction permet de sommer les éléments d'un vecteur et retourne le résultat (double).
+	La parallélisation de cette opération de sommation est assurée par réduction. Ainsi chaque thread
+	calcule un résultat partiel, puis le résultat final est calculé par sommation des résultats 
+	intermédiaires. 
+--------------------------------------------------------------------------------------------*/
+double sum_vector(const double Tab[], const int Taille_tab)
 {
-  int s = 0;
-  int i;
+  int s = 0; // Initialisation de la somme
+
+  /* ################### PARALLELISATION ################# */
   # pragma omp parallel for reduction (+:s)
-  //{
-  for (i = 0; i < Taille_tab; i++)
-  	{
+  /* ##################################################### */
+  for (int i = 0; i < Taille_tab; i++)
+  {
     s = s + Tab[i];
-  	}
-  //}
-  return s;
- 
+  }
+
+  return s; 
 }
 
-void multi_vector(double * v , const double  a ,  const int taille_v    )
-
-{	
-
-	{
-		# pragma omp parallel
-        # pragma omp for 
-	  	//{
- 		for (int i =0 ; i< taille_v  ; i++  ) 
- 			{
- 			
- 			v[i] = v[i] * a;
- 			}
-	 //	}
-   	}
-
-}
--- HPLC_TP1.h
+++ HPLC_TP1.h
@@ -1,6 +1,57 @@
-void fill(double* Tab , int Taille_tab, int min, int max );
-double Random (double min , double max);
-void display(const double* Tab, int Taille_tab);
-void sum_two_vector(double* v3 , const double * v1 , const double * v2 , const int taille_v1 , const int taille_v2  );
-double sum_vector(const double Tab[], int Taille_tab);
+/*--------------------- RESUME ---------------------------------------------------------
+Ce programme rassemble les fonctions et les procédures de l'exercice 1 sur les vecteurs.
+La mise en parréllélisation de ces dernières a également été implémentée. Ce programme a 
+été réalisé pour faciliter l'implémentation et les tests des fonctions et des procédures. 
+
+Dans ce fichier les fonctions et procédures sont triées par ordre alphabétique et succinte-
+ment présentées.
+---------------------------------------------------------------------------------------*/
+
+/*--------------------- Procédure : display  ------------------------------------------------
+	Cette procédure permet l'affichage d'un vecteur de taille définit.
+	Précondition : Le vecteur (Tab) doit être instancié, et la mémoire nécessaire allouée.
+--------------------------------------------------------------------------------------------*/
+void display(const double* Tab, const int Taille_tab);
+
+
+/*--------------------- Procédure : fill --------------------------------------------------
+	Cette procédure permet le remplissage d'un vecteur, de taille défini, par des doubles
+	générés aléatoirement compris entre min et max. La parallélisation de cette opératon est
+	assurée.
+	Précondition : Le vecteur (Tab) doit être instancié, et la mémoire nécessaire allouée.
+	Post-condition : La valeur des éléments du vecteur correspondent à un nombre aléatoire.
+------------------------------------------------------------------------------------------*/
+void fill(double* Tab , const int Taille_tab, const int min, const int max );
+
+
+/*--------------------- Fonction : Random  -------------------------------------------------
+	Cette fonction retourne un double généré alétoirement entre les bornes définies par min
+	er max. La fonction Random est appélé par la procédure fill.
+------------------------------------------------------------------------------------------*/
+double Random (const double min , const double max);
+
+
+/*--------------------- Procédure : multi_vector (Multiplication du vecteur par un double) --
+	Cette procédure permet de multiplier les éléments d'un vecteur par une constante (a).
+	Précondition:  - Le vecteur doit être instancié, et la mémoire nécessaire allouée.
+--------------------------------------------------------------------------------------------*/
 void multi_vector(double * v , const double  a ,  const int taille_v );
+
+
+/*--------------------- Procédure : sum_two_vector (Somme de deux vecteurs)  ----------------
+	Cette procédure permet de sommer deux vecteurs et retourne les résultats dans un vecteur v3.
+	Préconditions: - Les vecteurs doivent être instanciés, et la mémoire nécessaire allouée.
+				   - La taille des vecteurs à sommer (v1 et v2) doit être égale.
+	Postcondition: - Les éléments du vecteur v3 sont équivalents à la somme des éléments de v1
+					 et V2. 
+--------------------------------------------------------------------------------------------*/
+void sum_two_vector(double* v3 , const double * v1 , const double * v2 , const int taille_v1 , const int taille_v2 );
+
+
+/*--------------------- Fonction : sum_vector (Somme des éléments d'un vecteur)  ----------------
+	Cette fonction permet de sommer les éléments d'un vecteur et retourne le résultat (double).
+	Précondition:  - Le vecteur doit être instancié, et la mémoire nécessaire allouée.
+--------------------------------------------------------------------------------------------*/
+double sum_vector(const double Tab[], const int Taille_tab);
+
+
